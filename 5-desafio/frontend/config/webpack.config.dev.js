92700ea3e7bb924930e4bfbef48ade7167b28ce3
Nicolas Galiñanes <nicolas.galinianes@gmail.com>
2021-04-21 17:08:30 -0300

    Deshago un cambio previo en la url, vuelvo a 0.0.0.0

--- 5-desafio/frontend/config/webpack.config.dev.js
+++ 5-desafio/frontend/config/webpack.config.dev.js
@@ -15,11 +15,11 @@ const BundleTracker = require('webpack-bundle-tracker');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
-const publicPath = 'http://localhost:3000/';
+const publicPath = 'http://0.0.0.0:3000/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = 'http://localhost:3000/';
+const publicUrl = 'http://0.0.0.0:3000/';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
